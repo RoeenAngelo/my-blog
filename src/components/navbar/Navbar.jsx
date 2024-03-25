17897b482d07c08ce3fb1a04ae9dc1729c53ac9f
Roeen Acal <roeenacal@gmail.com>
2024-03-25 16:14:00 +0800

    add navbar social icons, logo, and links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,29 @@
 import React from "react";
 import styles from "./navbar.module.css";
+import Image from "next/image";
+import Link from "next/link";
+import AuthLinks from "../authLinks/AuthLinks";
+import ThemeToggle from "../themeToggle/ThemeToggle";
 
 const Navbar = () => {
-  return <div className={styles.container}>Navbar</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.social}>
+        <Image src="/facebook.png" alt="facebook" height={24} width={24} />
+        <Image src="/instagram.png" alt="instagram" height={24} width={24} />
+        <Image src="/tiktok.png" alt="tiktok" height={24} width={24} />
+        <Image src="/youtube.png" alt="youtube" height={24} width={24} />
+      </div>
+      <div className={styles.logo}>Roeen Angelo</div>
+      <div className={styles.links}>
+        <ThemeToggle />
+        <Link href="/">Home</Link>
+        <Link href="/">Contact</Link>
+        <Link href="/">About</Link>
+        <AuthLinks />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
