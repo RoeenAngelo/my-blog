48a5146d8670ed7471b3187a5fe6997b19a21ce3
Roeen Acal <roeenacal@gmail.com>
2024-03-27 14:02:22 +0800

    Create context folder and ThemeContext file for light and dark mode toggling

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -1,13 +1,19 @@
+"use client";
+
+import { ThemeContext } from "@/context/ThemeContext";
 import Image from "next/image";
-import React from "react";
+import React, { useContext } from "react";
 import styles from "./themeToggle.module.css";
 
 const ThemeToggle = () => {
+  const { theme } = useContext(ThemeContext);
+
+  console.log(theme);
   return (
     <div className={styles.container}>
-      <Image src="/moon.png" width={14} height={14} />
+      <Image src="/moon.png" alt="moon pic" width={14} height={14} />
       <div className={styles.ball}></div>
-      <Image src="/sun.png" width={14} height={14} />
+      <Image src="/sun.png" alt="sun pic" width={14} height={14} />
     </div>
   );
 };
