4310e699f13f4448a9518b9cb37a3de283c1ced0
Roeen Acal <roeenacal@gmail.com>
2024-03-28 17:36:41 +0800

    Add theme toggle ball effect

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -10,9 +10,24 @@ const ThemeToggle = () => {
 
   console.log(theme);
   return (
-    <div onClick={toggle} className={styles.container}>
+    <div
+      onClick={toggle}
+      className={styles.container}
+      style={
+        theme === "dark"
+          ? { backgroundColor: "white" }
+          : { backgroundColor: "#0f172a" }
+      }
+    >
       <Image src="/moon.png" alt="moon pic" width={14} height={14} />
-      <div className={styles.ball}></div>
+      <div
+        className={styles.ball}
+        style={
+          theme === "dark"
+            ? { left: 1, background: "#0f172a" }
+            : { right: 1, background: "white" }
+        }
+      ></div>
       <Image src="/sun.png" alt="sun pic" width={14} height={14} />
     </div>
   );
