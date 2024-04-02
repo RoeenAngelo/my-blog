e8d2b06f929b02d4fd2cd67397e815a8669000a7
Roeen Acal <roeenacal@gmail.com>
2024-04-02 11:57:40 +0800

    Add posts styling

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import styles from "./card.module.css";
 
@@ -10,8 +11,21 @@ const Card = () => {
       </div>
       <div className={styles.textContainer}>
         <div className={styles.textContainer}>
-          <span className={styles.date}>04/01/2023</span>
-          <span className={styles.category}>CULTURE</span>
+          <div className={styles.detail}>
+            <span className={styles.date}>04/01/2023 - </span>
+            <span className={styles.category}>CULTURE</span>
+          </div>
+          <Link href="/">
+            <h1 className={styles.title}>Lorem ipsum dolor sit amet.</h1>
+          </Link>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+            error voluptatibus quam unde nisi repudiandae harum recusandae magni
+            culpa cum!
+          </p>
+          <Link className={styles.link} href="/">
+            Read More
+          </Link>
         </div>
       </div>
     </div>
