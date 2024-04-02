490b9b9c960716d1066640a0542b00e65902b471
Roeen Acal <roeenacal@gmail.com>
2024-04-02 12:53:15 +0800

    Add side menu styling

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,8 +1,83 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import styles from "./menu.module.css";
 
 const Menu = () => {
-  return <div className={styles.container}>Menu</div>;
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.subtitle}>What's hot</h2>
+      <h1 className={styles.title}>Most Popular</h1>
+      <div className={styles.items}>
+        <Link href="/" className={styles.item}>
+          <div className={styles.imageContainer}>
+            <Image className={styles.image} src="/p1.jpeg" alt="" fill />
+          </div>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.travel}`}>
+              Travel
+            </span>
+            <h3 className={styles.postTitle}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>Roeen Angelo</span>
+              <span className={styles.date}> - 04/02/2024</span>
+            </div>
+          </div>
+        </Link>
+        <Link href="/" className={styles.item}>
+          <div className={styles.imageContainer}>
+            <Image className={styles.image} src="/p1.jpeg" alt="" fill />
+          </div>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.culture}`}>
+              Culture
+            </span>
+            <h3 className={styles.postTitle}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>Roeen Angelo</span>
+              <span className={styles.date}> - 04/02/2024</span>
+            </div>
+          </div>
+        </Link>
+        <Link href="/" className={styles.item}>
+          <div className={styles.imageContainer}>
+            <Image className={styles.image} src="/p1.jpeg" alt="" fill />
+          </div>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.food}`}>Food</span>
+            <h3 className={styles.postTitle}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>Roeen Angelo</span>
+              <span className={styles.date}> - 04/02/2024</span>
+            </div>
+          </div>
+        </Link>
+        <Link href="/" className={styles.item}>
+          <div className={styles.imageContainer}>
+            <Image className={styles.image} src="/p1.jpeg" alt="" fill />
+          </div>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.fashion}`}>
+              Fashion
+            </span>
+            <h3 className={styles.postTitle}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>Roeen Angelo</span>
+              <span className={styles.date}> - 04/02/2024</span>
+            </div>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Menu;
