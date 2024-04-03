b42da6b6d462f063bfbaa733833ddd6f9a9489e1
Roeen Acal <roeenacal@gmail.com>
2024-04-03 10:29:19 +0800

    Create SinglePage Component

---
+++ src/app/[slug]/page.jsx
@@ -0,0 +1,34 @@
+import Menu from "@/components/menu/Menu";
+import Image from "next/image";
+import React from "react";
+import styles from "./singlePage.module.css";
+
+const SinglePage = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.infoContainer}>
+        <div className={styles.textContainer}>
+          <h1 className={styles.title}>Lorem ipsum dolor sit.</h1>
+          <div className={styles.user}>
+            <div className={styles.userImageContainer}>
+              <Image src="/p1.jpeg" className={styles.image} alt="" fill />
+            </div>
+            <div className={styles.userTextContainer}>
+              <span className={styles.username}>Roeen</span>
+              <span className={styles.date}>04/03/2024</span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image src="/p1.jpeg" className={styles.image} alt="" fill />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <div className={styles.post}></div>
+        <Menu />
+      </div>
+    </div>
+  );
+};
+
+export default SinglePage;
