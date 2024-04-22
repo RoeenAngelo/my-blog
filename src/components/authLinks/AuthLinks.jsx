e37692b06ce8d08ea647d681af7a229d514b5114
Roeen Acal <roeenacal@gmail.com>
2024-04-22 17:14:25 +0800

    Revert "Add useSession hook to Authlinks.jsx"
    This reverts commit 0a2b0d993add517bd4c8cceffcbc400d1ba38f56.

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -2,16 +2,15 @@
 import Link from "next/link";
 import styles from "./authLinks.module.css";
 import { useState } from "react";
-import { signOut, useSession } from "next-auth/react";
 
 const AuthLinks = () => {
   const [open, setOpen] = useState(false);
 
-  const { status } = useSession();
+  const status = "notauthenticated";
 
   return (
     <>
-      {status === "unauthenticated" ? (
+      {status === "authenticated" ? (
         <Link href="/login" className={styles.link}>
           Login
         </Link>
@@ -20,9 +19,7 @@ const AuthLinks = () => {
           <Link href="/write" className={styles.link}>
             Write
           </Link>
-          <span className={styles.link} onClick={signOut}>
-            Logout
-          </span>
+          <span className={styles.link}>Logout</span>
         </>
       )}
       <div className={styles.burger} onClick={() => setOpen(!open)}>
